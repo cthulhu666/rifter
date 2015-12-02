e3fd2c213d97faac862b9d442f9b36e17b6b4b11
Jakub Głuszecki <jakub.gluszecki@gmail.com>
2015-12-02 19:15:45 +0100

    small/medium hybrid tracking speed traits (e.g. Tristan, Thorax)

--- lib/rifter/traits/xolap_fotef_bahez_zasem_samem_rutul_lopup_mivab_muriz_bemob_hozif_dylet_honaz_zozar_bylor_sidic_kaxox.rb
+++ lib/rifter/traits/xolap_fotef_bahez_zasem_samem_rutul_lopup_mivab_muriz_bemob_hozif_dylet_honaz_zozar_bylor_sidic_kaxox.rb
@@ -1,9 +1,16 @@
 module Rifter
   module Traits
     class XolapFotefBahezZasemSamemRutulLopupMivabMurizBemobHozifDyletHonazZozarBylorSidicKaxox < Trait
-      # TODO: generated class
       # used in 21 ships
-      # bonusText: "bonus to <a href=showinfo:3301>Small Hybrid Turret</a> tracking speed"
+      description 'bonus to <a href=showinfo:3301>Small Hybrid Turret</a> tracking speed'
+
+      def effect(fitting:, skill_lvl:)
+        fitting.boost_module_attribute(
+          -> (m) { m.ship_module.skill_required?('Small Hybrid Turret') },
+          :tracking_speed,
+          bonus * skill_lvl
+        )
+      end
     end
   end
 end
--- lib/rifter/traits/xubod_bered_ruteg_mosiz_cyboz_zehad_vifuk_benek_rotuv_tetih_lefip_povyd_tokah_hugek_nyvur_vokyk_baxyx.rb
+++ lib/rifter/traits/xubod_bered_ruteg_mosiz_cyboz_zehad_vifuk_benek_rotuv_tetih_lefip_povyd_tokah_hugek_nyvur_vokyk_baxyx.rb
@@ -1,9 +1,16 @@
 module Rifter
   module Traits
     class XubodBeredRutegMosizCybozZehadVifukBenekRotuvTetihLefipPovydTokahHugekNyvurVokykBaxyx < Trait
-      # TODO: generated class
       # used in 7 ships
-      # bonusText: "bonus to <a href=showinfo:3304>Medium Hybrid Turret</a> tracking speed"
+      description 'bonus to <a href=showinfo:3304>Medium Hybrid Turret</a> tracking speed'
+
+      def effect(fitting:, skill_lvl:)
+        fitting.boost_module_attribute(
+          -> (m) { m.ship_module.skill_required?('Medium Hybrid Turret') },
+          :tracking_speed,
+          bonus * skill_lvl
+        )
+      end
     end
   end
 end
