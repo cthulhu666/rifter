5a9dae01c1d23922e375bbc2cf32f80c9412fdde
Jakub Głuszecki <jakub.gluszecki@gmail.com>
2015-12-09 20:28:34 +0100

    another Gnosis bugfix

--- lib/rifter/traits/xuloc_nidok_zepyp_vugup_kofek_guzal_bibar_barac_lozeh_kabek_gosih_dalyp_gevef_sidoc_hydyn_fifuh_gexax.rb
+++ lib/rifter/traits/xuloc_nidok_zepyp_vugup_kofek_guzal_bibar_barac_lozeh_kabek_gosih_dalyp_gevef_sidoc_hydyn_fifuh_gexax.rb
@@ -4,9 +4,11 @@ module Rifter
       # used in 1 ships
       description 'bonus to <a href=showinfo:3306>Medium Energy Turret</a>, <a href=showinfo:3304>Medium Hybrid Turret</a> and <a href=showinfo:3305>Medium Projectile Turret</a> damage'
 
+      SKILLS = ['Medium Energy Turret', 'Medium Hybrid Turret', 'Medium Projectile Turret']
+
       def effect(fitting:, skill_lvl:)
         fitting.boost_module_attribute(
-          -> (m) { m.is_a?(ShipModule::Turret) },
+          -> (m) { SKILLS.any? { |s| m.item.skill_required?(s) } },
           :damage_multiplier,
           skill_lvl * bonus
         )
