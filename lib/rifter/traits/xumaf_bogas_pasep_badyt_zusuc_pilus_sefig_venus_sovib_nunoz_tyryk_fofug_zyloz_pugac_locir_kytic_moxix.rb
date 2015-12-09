2c441991bddcaa2cd5b1a400e60d641531e8dc13
Jakub Głuszecki <jakub.gluszecki@gmail.com>
2015-12-10 00:18:12 +0100

    fixed typo

--- lib/rifter/traits/xumaf_bogas_pasep_badyt_zusuc_pilus_sefig_venus_sovib_nunoz_tyryk_fofug_zyloz_pugac_locir_kytic_moxix.rb
+++ lib/rifter/traits/xumaf_bogas_pasep_badyt_zusuc_pilus_sefig_venus_sovib_nunoz_tyryk_fofug_zyloz_pugac_locir_kytic_moxix.rb
@@ -6,7 +6,7 @@ module Rifter
         'Missile Launcher Cruise', 'Missile Launcher Torpedo'
       ]
 
-      descrpition 'bonus to <a href=showinfo:3324>Heavy Missile</a>, <a href=showinfo:3326>Cruise Missile</a> and <a href=showinfo:3325>Torpedo</a> damage'
+      description 'bonus to <a href=showinfo:3324>Heavy Missile</a>, <a href=showinfo:3326>Cruise Missile</a> and <a href=showinfo:3325>Torpedo</a> damage'
 
       def effect(fitting:, skill_lvl:)
         fitting.boost_module_attribute(
