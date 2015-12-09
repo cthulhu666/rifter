f2eda85207f79d3a30cc1d5d325cd59f146a3120
Jakub Głuszecki <jakub.gluszecki@gmail.com>
2015-12-09 19:49:04 +0100

    fixed Gnosis turrets trait

--- lib/rifter/traits/xuloc_nidok_zepyp_vugup_kofek_guzal_bibar_barac_lozeh_kabek_gosih_dalyp_gevef_sidoc_hydyn_fifuh_gexax.rb
+++ lib/rifter/traits/xuloc_nidok_zepyp_vugup_kofek_guzal_bibar_barac_lozeh_kabek_gosih_dalyp_gevef_sidoc_hydyn_fifuh_gexax.rb
@@ -4,7 +4,7 @@ module Rifter
       # used in 1 ships
       description 'bonus to <a href=showinfo:3306>Medium Energy Turret</a>, <a href=showinfo:3304>Medium Hybrid Turret</a> and <a href=showinfo:3305>Medium Projectile Turret</a> damage'
 
-      def fittings(fitting:, skill_lvl:)
+      def effect(fitting:, skill_lvl:)
         fitting.boost_module_attribute(
           -> (m) { m.is_a?(ShipModule::Turret) },
           :damage_multiplier,
